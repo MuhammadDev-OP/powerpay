16fd302bab244f843578cd7c50c4357e49d1323c
Muhammad Bangash <muhammadbangash190@gmail.com>
2024-03-25 16:30:45 +0500

    Content push

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/widgets/Hero";
+import Content from "../components/widgets/Content";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <Content />
     </>
   );
 };
